27060d21b74dca27c86613726adacc7ddf504213
Melissa <melissa_parra@usmp.pe>
2021-10-12 17:38:54 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Simulacro_PC3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
